f241dad087f439fb49c7f79a3903a4f3b5cfbff4
Iulia-Don <132606822+Iulia-Don@users.noreply.github.com>
2023-05-22 21:30:00 +0300

    add calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[]args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
 
+        System.out.println( "сложение: "+ addition(number1, number2));
+        System.out.println( "вычитание: "+ subtraction(number1, number2));
+        System.out.println( "умножение: "+ multiplication(number1, number2));
+        System.out.println( "деление: "+ division(number1, number2));
+    }
+    public static int addition(int x, int y){
+        return x+y;
+    }
+    public static int subtraction(int x, int y){
+        return x-y;
+    }
+    public static int  multiplication(int x, int y){
+        return x*y;
+    }
+    public static double division(int x, int y){
+        return (double)  x/y;
     }
 }
