1afe265e0e63493dd631d4a2c577e6b5f7d3ae5f
Iulia-Don <132606822+Iulia-Don@users.noreply.github.com>
2023-06-05 21:39:31 +0300

    Homework commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
-
+        int count =1;
+        while(true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+             if (fileExists==false || isDirectory){
+            System.out.println("файл не существует или указанный путь является путём к папке");
+            continue;
+            }
+            else if(fileExists && isDirectory==false) {
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер "+count);
+            }
+      }
     }
 }
